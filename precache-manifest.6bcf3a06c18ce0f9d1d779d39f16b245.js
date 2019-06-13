c8e2fba994d3d29d98067b9015b27098bac2e1bf
kleyber rojas <bonarja@gmail.com>
2019-06-13 15:44:33 -0400

    Updates

---
+++ precache-manifest.6bcf3a06c18ce0f9d1d779d39f16b245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfa7b81a69e7d1744e2fa2cb779fc99",
+    "url": "/sdom/index.html"
+  },
+  {
+    "revision": "4e5f5e4f5392ed75030b",
+    "url": "/sdom/static/css/main.ed4cfdcf.chunk.css"
+  },
+  {
+    "revision": "aadd9a763aaa95e6ccad",
+    "url": "/sdom/static/js/2.8036f88a.chunk.js"
+  },
+  {
+    "revision": "4e5f5e4f5392ed75030b",
+    "url": "/sdom/static/js/main.8c636d84.chunk.js"
+  },
+  {
+    "revision": "bf7c1c5d28ddf6c08dad",
+    "url": "/sdom/static/js/runtime~main.22291f01.js"
+  },
+  {
+    "revision": "3ce2bfe219c599a6ec3305b3a48e980d",
+    "url": "/sdom/static/media/menu.3ce2bfe2.svg"
+  }
+]);
